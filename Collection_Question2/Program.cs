5b8782bb0cdc54bfddad7025022034b2f71ea7ae
swapnilgaikwad18 <swapnil1329g@gmail.com>
2023-06-20 00:05:03 +0530

    Question 2 of Collection is Done.

---
+++ Collection_Question2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*  Create an array of Employee objects. 
+ *  Convert it to a List<Employee>.  
+ *  Display all the Employees in the list.*/
+namespace Collection_Question2
+{
+    internal class Program
+    {
+        class Employee
+        {
+            public int EmpId { get; set; }
+            public string Name { get; set; }
+            public int Salary { get; set; }
+        }
+        static void Main()
+        {
+            Employee[] empArr = new Employee[]
+            {
+                new Employee {EmpId = 101, Name = "Swapnil", Salary = 75000},
+                new Employee {EmpId = 102, Name = "Sahil", Salary = 100000},
+                new Employee {EmpId = 103, Name = "Swapnil", Salary = 50000}
+            };
+
+            List<Employee> empList = new List<Employee>();
+
+            foreach (Employee emp in empArr)
+            {
+                Console.WriteLine($"Details of all employees: ");
+                Console.WriteLine($"Employee Name: {emp.EmpId}");
+                Console.WriteLine($"Employee Name: {emp.Name}");
+                Console.WriteLine($"Employee Salary: {emp.Salary}");
+            }
+        }
+    }
+}
