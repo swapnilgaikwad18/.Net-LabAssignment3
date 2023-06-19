3489618bca169b17d091932c5301aab43ca0e5a6
swapnilgaikwad18 <swapnil1329g@gmail.com>
2023-06-19 17:46:09 +0530

    Add project files.

---
+++ Array_Question1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*1. CDAC Kochi/Tri has certain number of batches. each batch has certain number of students.
+        accept number of batches. for each batch accept number of students.
+        create an array to store mark for each student (1 student has only 1 subject mark)
+        accept the marks.
+        display the marks.*/
+
+namespace Array_Question1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
