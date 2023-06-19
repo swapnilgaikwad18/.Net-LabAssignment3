b903b447e38c88ba89790d93d9d1a2861efd5ce0
swapnilgaikwad18 <swapnil1329g@gmail.com>
2023-06-19 21:00:27 +0530

    Question1 of array Done!

--- Array_Question1/Program.cs
+++ Array_Question1/Program.cs
@@ -8,10 +8,38 @@ namespace Array_Question1
 {
     internal class Program
     {
+       
         static void Main()
         {
+            Console.WriteLine("Enter the number of batches: ");
+            int noOfBatches = Convert.ToInt32(Console.ReadLine());
+            
+            int[][] arr = new int[noOfBatches][];
 
-            Console.WriteLine("Hello, World!");
+            for(int i = 0; i < noOfBatches; i++)
+            {
+                Console.WriteLine($"Enter number of students in batch{i+1}: ");
+                int numOfStudents = Convert.ToInt32(Console.ReadLine());
+
+                arr[i] = new int[numOfStudents];
+
+                for(int j = 0; j < numOfStudents; j++)
+                {
+                    Console.WriteLine($"Enter marks for student{j+1} in batch{i + 1}: ");
+                    arr[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+
+            for(int i = 0; i < noOfBatches; i++)
+            {
+                Console.WriteLine($"batch{i}: ");
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.WriteLine($"Student{j + 1} : {arr[i][j]}");
+                }
+            }
         }
     }
 }
